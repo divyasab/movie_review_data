f0b94ab7bc5846f763dabea70066be7ffddeaf8d
divya sable <divyasable21@navgurukul.org>
2023-07-26 14:37:22 +0530

    This project i done Header

---
+++ app/Header.js
@@ -0,0 +1,32 @@
+const Header = () => {
+    return (
+        <>
+            <div className='Header'></div>
+            <div className='Header1'>
+                <div className='col0 col'>
+                    <img src="Movie_hub.png"/> 
+                </div>
+                <div className='col1 col'>
+                    <h1>Movie Hub</h1>
+                </div>
+                <div className='col2 col'>
+                    <h2>Add movie</h2>
+                </div>
+                <div className='col3 col'>
+                    <h2>Watch</h2>
+                </div>
+            </div>
+            <div className='Header2'></div>
+            <div className='Header3'></div>
+            <div className='Header4'>
+                <div className="col4 col">
+                    <div className="col5 col">
+                        <h2>Watch</h2>
+                    </div>
+                </div>
+            </div>
+       </>
+
+     )
+}
+export default Header;
---
+++ app/page.js
@@ -0,0 +1,16 @@
+import Header from "./Header.js";
+import "./globals.css";
+
+const box = () => {
+    return(
+        <>
+            <Header/>
+
+        </>
+
+    
+    
+    )
+}
+export default box;
+
