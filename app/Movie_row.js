5ae7306ae1361831e63d8e3d057ec3a48bdb26bc
divya sable <divyasable21@navgurukul.org>
2023-07-26 15:02:01 +0530

    Movie_row is done

---
+++ app/Movie_row.js
@@ -0,0 +1,17 @@
+export default function Movie_row(){
+    return(
+        <div className="Movie_row1">
+            <div className="image_logo">
+            </div>
+            <div className="Movie_details">
+                <h2></h2>
+                <p></p>
+                <h3></h3>
+                <p></p>
+            </div>
+        </div>
+            
+    );
+};
+            
+
--- app/page.js
+++ app/page.js
@@ -1,10 +1,28 @@
 import Header from "./Header.js";
+import Movie_row from "./Movie_row.js";
 import "./globals.css";
 
 const box = () => {
     return(
         <>
             <Header/>
+                <Movie_row/>
+                <Movie_row/>
+                <Movie_row/>
+                <Movie_row/>
+                <Movie_row/>
+                <Movie_row/>
+                <Movie_row/>
+                <Movie_row/>
+                <Movie_row/>
+                <Movie_row/>
+
+
+
+
+
+
+
 
         </>
 
